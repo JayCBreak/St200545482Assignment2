ab0a55c265c27a895ed934700d35d906e5f0170a
Jacob Chotenovsky <github@jacobchotenovsky.com>
2023-11-16 18:54:31 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st200545482assignment2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st200545482assignment2 to javafx.fxml;
+    exports com.example.st200545482assignment2;
+}
